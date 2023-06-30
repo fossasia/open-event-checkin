90e576208b43afe794d2a03d3280c5bc761c0cb2
lizardon <donchia@ymail.com>
2023-07-01 02:03:45 +0800

    QRCode View init

--- src/components/NotificationView.vue
+++ src/components/NotificationView.vue
@@ -69,9 +69,7 @@ watch(
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div
-          class="flex min-h-full items-center justify-center p-4 text-center sm:p-0"
-        >
+        <div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
           <TransitionChild
             as="template"
             enter="ease-out duration-300"
