68eb898c53fd7c8c92e991dcf23c5ac84f03e7e6
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-07-09 23:16:45 +0800

    Update BaseTemplate.vue

--- src/components/Registration/Manual/BaseTemplate.vue
+++ src/components/Registration/Manual/BaseTemplate.vue
@@ -30,7 +30,7 @@ const tabs = [
           tab.current ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700',
           'rounded-md px-3.5 py-2.5 text-sm font-medium inline-flex items-center'
         ]"
-        @click="changeTab(tab)" 
+        @click="changeTab(tab)"
         :aria-current="tab.current ? 'page' : undefined"
       >
         <component :is="tab.icon" class="h-6 w-6 flex-none mr-2" aria-hidden="true" />
