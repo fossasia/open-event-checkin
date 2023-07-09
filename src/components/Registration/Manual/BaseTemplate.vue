b141486bcccef33dc2ee1cdedc0bf3589a89446c
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-07-09 23:15:27 +0800

    Fixed navabr

--- src/components/Registration/Manual/BaseTemplate.vue
+++ src/components/Registration/Manual/BaseTemplate.vue
@@ -5,9 +5,15 @@ import { CameraIcon, ChartBarIcon, RocketLaunchIcon } from '@heroicons/vue/24/ou
 const route = useRoute()
 const eventId = route.params.eventId
 
+const changeTab = (tab) => {
+  tabs.forEach((element) => {
+    if (element !== tab) element.current = false
+    else element.current = true
+  })
+}
+
 const tabs = [
-  { name: 'Search', href: 'registerScan', icon: CameraIcon, current: true },
-  { name: 'Hybrid', href: 'registerHybrid', icon: RocketLaunchIcon, current: false },
+  { name: 'Hybrid', href: 'registerHybrid', icon: RocketLaunchIcon, current: true },
   { name: 'Stats', href: 'registerStats', icon: ChartBarIcon, current: false }
 ]
 </script>
@@ -24,6 +30,7 @@ const tabs = [
           tab.current ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700',
           'rounded-md px-3.5 py-2.5 text-sm font-medium inline-flex items-center'
         ]"
+        @click="changeTab(tab)" 
         :aria-current="tab.current ? 'page' : undefined"
       >
         <component :is="tab.icon" class="h-6 w-6 flex-none mr-2" aria-hidden="true" />
