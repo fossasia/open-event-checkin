4110a9f79c4a7414c8a299e312371e5ae97fc675
lizardon <donchia@ymail.com>
2023-07-14 03:03:54 +0800

    Fix #30

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,8 +1,47 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useLoadingStore } from '@/stores/loading'
+import { useAuthStore } from '@/stores/auth'
 
-function submitForm() {
-  console.log('submitting form')
+// form fields
+const email = ref('')
+const password = ref('')
+
+// error handling
+const showError = ref(false)
+
+// stores
+const loadingStore = useLoadingStore()
+const authStore = useAuthStore()
+
+// router
+const router = useRouter()
+const route = useRoute()
+
+async function submitLogin() {
+  loadingStore.show = true
+
+  const payload = {
+    email: email.value,
+    password: password.value
+  }
+
+  await authStore
+    .login(payload, 'auth/login')
+    .then(async (res) => {
+      localStorage.setItem('token', Object(res).access_token)
+      showError.value = false
+      loadingStore.show = false
+      router.push({
+        name: 'stationSelector'
+      })
+    })
+    .catch((err) => {
+      loadingStore.show = false
+      console.log(err)
+      showError.value = true
+    })
 }
 </script>
 
@@ -12,7 +51,7 @@ function submitForm() {
       <h2 class="mt-0 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2>
-      <form class="space-y-6 mt-10" @submit.prevent="submitForm">
+      <form class="space-y-6 mt-10" @submit.prevent="submitLogin">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
             >Email address</label
@@ -20,6 +59,7 @@ function submitForm() {
           <div class="mt-2">
             <input
               id="email"
+              v-model="email"
               name="email"
               type="email"
               autocomplete="email"
@@ -36,6 +76,7 @@ function submitForm() {
           <div class="mt-2">
             <input
               id="password"
+              v-model="password"
               name="password"
               type="password"
               autocomplete="current-password"
@@ -46,16 +87,20 @@ function submitForm() {
         </div>
 
         <div>
-          <RouterLink
+          <!-- <RouterLink
             :to="{ name: 'stationSelector', params: { scannerType: 'checkin', eventId: '123' } }"
+          > -->
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
-            <button
-              type="submit"
-              class="flex w-full justify-center rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-            >
-              Login
-            </button>
-          </RouterLink>
+            Login
+          </button>
+          <!-- </RouterLink> -->
+        </div>
+
+        <div v-if="showError">
+          <p class="text-sm text-red-500">Wrong credentials or account does not exist</p>
         </div>
       </form>
 
