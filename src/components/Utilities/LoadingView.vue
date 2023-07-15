4bede01116c284107e4d085dbdf97b3fe798041b
lizardon <donchia@ymail.com>
2023-07-15 12:04:46 +0800

    Updated spinner to tailwind

--- src/components/Utilities/LoadingView.vue
+++ src/components/Utilities/LoadingView.vue
@@ -4,33 +4,7 @@
     class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center"
   >
     <div
-      class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"
+      class="w-12 h-12 rounded-full animate-spin border-2 border-solid border-blue-600 border-t-transparent"
     ></div>
   </div>
 </template>
-
-<style scoped>
-.loader {
-  border-top-color: #4a4af4;
-  -webkit-animation: spinner 1.5s linear infinite;
-  animation: spinner 1.5s linear infinite;
-}
-
-@-webkit-keyframes spinner {
-  0% {
-    -webkit-transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-  }
-}
-
-@keyframes spinner {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
