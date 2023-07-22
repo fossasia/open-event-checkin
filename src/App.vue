88a8143d791a59082635ef2faae157c4b815274b
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-07-22 08:21:35 +0800

    removed grow flex

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,6 @@ import LoadingView from '@/components/Utilities/LoadingView.vue'
 </script>
 
 <template>
-  <div class="flex flex-col h-screen">
-    <div class="grow flex">
-      <RouterView class="grow m-6 flex flex-col" />
-    </div>
-  </div>
+  <RouterView class="m-6 h-screen" />
   <LoadingView v-if="useLoadingStore().show"></LoadingView>
 </template>
