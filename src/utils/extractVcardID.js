9a7941df8ad320d0fc7905be0e685815212afb2b
cweitat <cweitat@gmail.com>
2023-09-06 22:47:10 +0800

    create new utils

---
+++ src/utils/extractVcardID.js
@@ -0,0 +1,14 @@
+export default function extractVcardID(vcard) {
+  // check if EVENTYAY key exist in vcard
+  if (!vcard.includes('EVENTYAY')) {
+    return false
+  }
+  // split vcard into array
+  const vcardArray = vcard.split('\n')
+  // find the index of EVENTYAY key
+  const index = vcardArray.findIndex((item) => item.includes('EVENTYAY:'))
+  // get value from EVENTYAY key
+  const id = vcardArray[index].split(':')[1]
+ 
+  return id ? id : false
+}
